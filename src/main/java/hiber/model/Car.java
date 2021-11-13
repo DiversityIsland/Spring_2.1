7e218b70450a7a0a07915e99e0580e3a97be5a1f
DiversityIsland <osh.gaziz@gmail.com>
2021-11-13 22:54:16 +0300

    resolve_1

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,56 @@
+package hiber.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "series")
+    private int series;
+
+    @Column(name = "model")
+    private String model;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {}
+
+    public Car( String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser() {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "series: " + series + ", model: " + model;
+    }
+}
